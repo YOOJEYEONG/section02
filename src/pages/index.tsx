2c97882c3b37cf3f404a00b72d4e33c578216361
jeyoung <jdngh@naver.com>
2025-11-03 23:56:40 +0900

    study : SSR 실습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,39 @@
 // CSS Module
 import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
-import books from "@/mock/books.json";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 import { InferGetServerSidePropsType } from "next";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import style from "./index.module.css";
 
 export const getServerSideProps = async () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
-
-  const data = "hello";
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
   return {
-    props: { data },
+    props: { allBooks, recoBooks },
   };
 };
 
 export default function Home({
-  data,
+  allBooks,
+  recoBooks,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data);
-
-  useEffect(() => {
-    console.log(window);
-  }, []);
-
+  console.log(allBooks);
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
